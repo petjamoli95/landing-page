12a7eb571eeb92177d244c036e051dbf05dcb25d
Pete <petjamoli95@outlook.com>
2024-09-06 00:37:18 +0100

    Removed RoomCards component

--- src/components/RoomCard.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function RoomCard({ room }) {
-  return (
-    <div className="card">
-      <Image src={`http://35.179.72.232${room.mainImage.meta.download_url}`} width={1274} height={700} alt={room.mainImage.title} />
-    </div>
-  ) 
-}
