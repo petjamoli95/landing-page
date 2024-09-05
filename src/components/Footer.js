5b7ff5e809f2376a2b4c60178880cf492c02828d
Pete <petjamoli95@outlook.com>
2024-09-06 00:36:32 +0100

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className='flex items-center font-karla justify-end px-8 py-12 bg-grey w-full'>
       <div>
-        <p>The Hugo 2024 | Privacy Policy</p>
+        <p>© The Hugo 2024 | <a>Privacy Policy</a></p>
       </div>
     </div>
   )
