d3ff591e4af4f44b581af94d4311e1b345aa48f5
Pete <petjamoli95@outlook.com>
2024-09-08 20:19:27 +0100

    Increade Button size

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 export default function Button() {
   return (
     <div>
-      <button className='p-8 font-karla text-2xl text-bold bg-red text-white'>BOOK</button>
+      <button className='p-10 font-karla text-2xl text-bold bg-red text-white'>BOOK</button>
     </div>
   )
 }
